2e5ce5fac24e055314d931103b59389b66db7d74
Justin Vicente <javicent@ucsc.edu>
2023-10-18 10:01:01 -0700

    Step 2

--- src/main.ts
+++ src/main.ts
@@ -2,15 +2,32 @@ import "./style.css";
 
 const app: HTMLDivElement = document.querySelector("#app")!;
 
-const gameName = "JSTN's game";
-
+const gameName: string = "JSTN's game";
 document.title = gameName;
 
-const header = document.createElement("h1");
+const header: HTMLHeadingElement = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
 
 // Step 1
-const button = document.createElement("button");
+const button: HTMLButtonElement = document.createElement("button");
 button.innerHTML = "🦍";
 app.append(button);
+
+// Step 2
+let counter: number = 0;
+
+const counterDisplay: HTMLDivElement = document.createElement("div");
+counterDisplay.innerHTML = `${counter} ${getUnitLabel()}`;
+app.append(counterDisplay);
+
+function getUnitLabel(): string {
+  return "cookies"; // You can choose a fun unit label relevant to your emoji
+}
+
+function updateCounter() {
+  counter++;
+  counterDisplay.innerHTML = `${counter} ${getUnitLabel()}`;
+}
+
+button.addEventListener("click", updateCounter);
