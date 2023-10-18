82a5fbe480bace99015931dad59d0f321482940a
Justin Vicente <javicent@ucsc.edu>
2023-10-18 09:57:01 -0700

    Step  1

--- src/main.ts
+++ src/main.ts
@@ -9,3 +9,8 @@ document.title = gameName;
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
+
+// Step 1
+const button = document.createElement("button");
+button.innerHTML = "🦍";
+app.append(button);
