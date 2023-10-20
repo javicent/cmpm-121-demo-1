3c3037e9b1060b676212d6c6c49218668f26703b
Justin Vicente <javicent@ucsc.edu>
2023-10-20 12:32:18 -0700

    S

--- src/main.ts
+++ src/main.ts
@@ -9,12 +9,6 @@ const header: HTMLHeadingElement = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
 
-// Step 1
-const button: HTMLButtonElement = document.createElement("button");
-button.innerHTML = "🦍";
-app.append(button);
-
-// Step 2
 let counter: number = 0;
 
 const counterDisplay: HTMLDivElement = document.createElement("div");
@@ -25,49 +19,22 @@ function getUnitLabel(): string {
   return "Recruited Monkeys";
 }
 
-// Step 3
-setInterval(() => {
-  counter++;
-  counterDisplay.innerHTML = `${counter} ${getUnitLabel()}`;
-}, 1000);
-
-// Step 4
 let lastTimestamp: number = 0;
 
+const button: HTMLButtonElement = document.createElement("button");
+app.append(button);
+button.innerHTML = "🦍";
+
 button.addEventListener("click", () => {
-    counter++;
-    counterDisplay.innerHTML = `${counter.toFixed(2)} ${getUnitLabel()}`;
+  counter++;
+  counterDisplay.innerHTML = `${counter.toFixed(2)} ${getUnitLabel()}`;
 });
 
 requestAnimationFrame(updateCounter);
 
-// Step 5
-//let growthRate: number = 0;
-
-/* Not needed anymore
-const upgradeButton: HTMLButtonElement = document.createElement("button");
-upgradeButton.innerHTML = "Deploy 10 Monkeys for upgrade";
-app.append(upgradeButton);
-
-upgradeButton.disabled = true;
-
-upgradeButton.addEventListener("click", () => {
-  if (counter >= 10) {
-    counter -= 10;
-    growthRate++;
-    upgradeButton.disabled = true;
-  }
-});
-*/
-
-// Step 6
 let totalGrowthRate: number = 0;
 
-let itemCounts: Record<string, number> = {
-    A: 0,
-    B: 0,
-    C: 0,
-};
+const itemCounts: Record<string, number> = {};
 
 const growthRateDisplay: HTMLDivElement = document.createElement("div");
 app.append(growthRateDisplay);
@@ -76,59 +43,85 @@ const itemCountsDisplay: HTMLDivElement = document.createElement("div");
 app.append(itemCountsDisplay);
 
 function updateCounter(timestamp: number) {
-    const timeElapsed = timestamp - lastTimestamp;
-  
-    // Calculate the total growth rate based on items purchased
-    const totalGrowthRate = calculateTotalGrowthRate();
-  
-    // Calculate the fractional increase based on the total growth rate
-    const fractionalIncrease = (timeElapsed / 1000) * totalGrowthRate;
-    counter += fractionalIncrease;
-  
-    // Update the counter display
-    counterDisplay.innerHTML = `${counter.toFixed(2)} ${getUnitLabel()}`;
-  
-    // Update the growth rate display
-    growthRateDisplay.innerHTML = `${totalGrowthRate.toFixed(1)} ${getUnitLabel()}/sec`;
-  
-    // Update the item counts display
-    itemCountsDisplay.innerHTML = `Items Purchased: A(${itemCounts.A}), B(${itemCounts.B}), C(${itemCounts.C})`;
-  
-    lastTimestamp = timestamp;
-    requestAnimationFrame(updateCounter);
+  const timeElapsed = timestamp - lastTimestamp;
+
+  // Calculate the total growth rate based on items purchased
+  const totalGrowthRate = calculateTotalGrowthRate();
+
+  // Calculate the fractional increase based on the total growth rate
+  const fractionalIncrease = (timeElapsed / 1000) * totalGrowthRate;
+  counter += fractionalIncrease;
+
+  // Update the counter display
+  counterDisplay.innerHTML = `${counter.toFixed(2)} ${getUnitLabel()}`;
+
+  // Update the growth rate display
+  growthRateDisplay.innerHTML = `${totalGrowthRate.toFixed(1)} ${getUnitLabel()}/sec`;
+
+  // Update the item counts display
+  itemCountsDisplay.innerHTML = generateItemCountsDisplay();
+
+  lastTimestamp = timestamp;
+  requestAnimationFrame(updateCounter);
 }
 
-const upgradeItems: Record<string, { cost: number; growthRate: number; costMultiplier: number}> = {
-    "Private Gorilla": { cost: 10, growthRate: 0.1, costMultiplier: 1.15 },
-    "Lieutenant Macaque": { cost: 100, growthRate: 2.0, costMultiplier: 1.15 },
-    "Captain Dryomomys": { cost: 1000, growthRate: 50.0, costMultiplier: 1.15 },
-};
+// Define the available items
+interface Item {
+  name: string;
+  cost: number;
+  rate: number;
+  costMultiplier: number; // Add costMultiplier property
+}
 
-  function calculateTotalGrowthRate(): number {
-    let totalRate = 0;
-    for (const item in itemCounts) {
-      totalRate += itemCounts[item] * upgradeItems[item].growthRate;
+const availableItems: Item[] = [
+  { name: "Private Gorilla", cost: 10, rate: 0.1, costMultiplier: 1.15 },
+  { name: "Lieutenant Macaque", cost: 100, rate: 2.0, costMultiplier: 1.15 },
+  { name: "Captain Dryomomys", cost: 1000, rate: 50.0, costMultiplier: 1.15 },
+];
+
+function calculateTotalGrowthRate(): number {
+  let totalRate = 0;
+  for (const item in itemCounts) {
+    const selectedItem = availableItems.find((i) => i.name === item);
+    if (selectedItem) {
+      totalRate += itemCounts[item] * selectedItem.rate;
     }
-    return totalRate;
+  }
+  return totalRate;
 }
 
 function purchaseUpgrade(itemName: string) {
-    if (counter >= upgradeItems[itemName].cost) {
-        counter -= upgradeItems[itemName].cost;
-        totalGrowthRate += upgradeItems[itemName].growthRate;
-        itemCounts[itemName]++;
-        upgradeItems[itemName].cost *= upgradeItems[itemName].costMultiplier;
+  const selectedItem = availableItems.find((item) => item.name === itemName);
+
+  if (selectedItem && counter >= selectedItem.cost) {
+    counter -= selectedItem.cost;
+    totalGrowthRate += selectedItem.rate;
+    if (itemCounts[itemName] === undefined) {
+      itemCounts[itemName] = 1;
+    } else {
+      itemCounts[itemName]++;
     }
+    selectedItem.cost *= selectedItem.costMultiplier;
+  }
 }
 
-// Step 7
-for (const item in upgradeItems) {
-    const button: HTMLButtonElement = document.createElement("button");
-    const cost = upgradeItems[item].cost.toFixed(2);
-    button.innerHTML = `Level Up ${item} (${cost} Monkeys, ${upgradeItems[item].growthRate.toFixed(2)} ${getUnitLabel()}/sec)`;
-    app.append(button);
-    button.addEventListener("click", () => {
-      purchaseUpgrade(item);
-      button.innerHTML = `Purchase ${item} (${upgradeItems[item].cost.toFixed(2)} units, ${upgradeItems[item].growthRate.toFixed(2)} ${getUnitLabel()}/sec)`;
-    });
+function generateItemCountsDisplay(): string {
+  let displayText = "Items Purchased: ";
+  for (const item of availableItems) {
+    const itemCount = itemCounts[item.name] || 0;
+    displayText += `${item.name}(${itemCount}), `;
+  }
+  return displayText.slice(0, -2);
+}
+
+// Buttons to purchase upgrades
+for (const item of availableItems) {
+  const button: HTMLButtonElement = document.createElement("button");
+  const cost = item.cost.toFixed(2);
+  button.innerHTML = `Level Up ${item.name} (${cost} units, ${item.rate.toFixed(2)} ${getUnitLabel()}/sec)`;
+  app.append(button);
+  button.addEventListener("click", () => {
+    purchaseUpgrade(item.name);
+    button.innerHTML = `Level Up ${item.name} (${item.cost.toFixed(2)} units, ${item.rate.toFixed(2)} ${getUnitLabel()}/sec)`;
+  });
 }
